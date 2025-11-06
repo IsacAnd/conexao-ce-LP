c04e79cf5750508ea8fe5e5cd989485c8030bca1
IsacAnd <grimjaeguer@gmail.com>
2025-11-06 14:44:42 -0300

    location section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Schedule from "@/components/Schedule";
+import Location from "@/components/Location";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       <Hero />
 
       <Schedule />
+
+      <Location />
 
       <Footer />
     </div>
